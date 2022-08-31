58c192dffa71bcacf03629f4224ef8107a3c23ba
Георгий Дерябин <gderyabin@mtsbank.ru>
2022-08-31 20:13:15 +0300

    sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://7b3beb4564e149d3a2817aa5bd199d2d@o1386496.ingest.sentry.io/6706861",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 import "@public/css/bootstrap.min.css";
 // import "animate.css";
 import "@public/css/boxicons.min.css";
