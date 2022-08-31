eab4e74289073528ceb6165f07a899bf215c3270
Георгий Дерябин <gderyabin@mtsbank.ru>
2022-08-31 17:05:40 +0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     // open: "http://0.0.0.0:80",
     host: "0.0.0.0",
